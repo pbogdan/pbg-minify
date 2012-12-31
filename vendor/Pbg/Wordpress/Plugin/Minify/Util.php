038dc9842c470cca5e6c3796739de8fc0ef4df0d
Piotr Bogdan <ppbogdan@gmail.com>
2012-12-31 16:38:49 +0000

    Add Util class documentation.

--- vendor/Pbg/Wordpress/Plugin/Minify/Util.php
+++ vendor/Pbg/Wordpress/Plugin/Minify/Util.php
@@ -1,9 +1,39 @@
 <?php
 
+/**
+ * Misc utilities.
+ *
+ * PHP version 5
+ *
+ * @category Pbg\Wordpress\Plugin
+ * @package  Pbg\Wordpress\Plugin\Minify
+ * @author   Piotr Bogdan <ppbogdan@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://github.com/pbogdan/pbg-minify
+ */
+
 namespace Pbg\Wordpress\Plugin\Minify;
 
+/**
+ * Misc utilities.
+ *
+ * Main purpose is to set up plugin activation / deactivation hooks.
+ *
+ * @category Pbg\Wordpress\Plugin
+ * @package  Pbg\Wordpress\Plugin\Minify
+ * @author   Piotr Bogdan <ppbogdan@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://github.com/pbogdan/pbg-minify
+ */
 class Util
 {
+    /**
+     * Translate an URL to a file-system location within Wordpress context.
+     *
+     * @param string $url URL
+     *
+     * @return string
+     */
     public static function urlToPath($url)
     {
         if (strpos($url, \content_url()) !== false) {
